4e29f0f856ceeb33625043a44da12d464ba9281c
daesung <39398625+sung029@users.noreply.github.com>
2019-07-18 14:34:32 +0900

    d

--- js/script.js
+++ js/script.js
@@ -336,7 +336,7 @@ $(document).ready(function() {
 	// 07. COUNTDOWN
 	//===================================================================================
 	var theday = new Date();
-	theday = new Date(2015, 7, 12);
+	theday = new Date(2019, 8, 22);
 	$('#countdown').countdown({until: theday, format: 'WDHMS'});
 	$('#countdown').countdown($.countdown.regionalOptions['custom-label']); 
 		 
